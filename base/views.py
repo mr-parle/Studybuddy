38a674ab7ce7d6ea19b8ff40688d85577656c30c
mr-parle <kumarparlevinay@gmail.com>
2024-03-21 01:10:28 +0530

    .

--- base/views.py
+++ base/views.py
@@ -7,7 +7,13 @@ rooms = [
 ]
 # Create your views here.
 def home(request):
-    return render(request, 'home.html', {'room': rooms})
+    context = {'rooms': rooms}
+    return render(request, 'base/home.html', context )
 
-def room(request):
-    return render(request, 'room.html')
+def room(request, pk):
+    rooom = None
+    for i in rooms:
+        if i['id'] == int(pk):
+            room=i
+    context= {'room':room}
+    return render(request, 'base/room.html', context)
