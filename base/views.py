7df352d96ca2a8623d95b981915e455702966b26
mr-parle <kumarparlevinay@gmail.com>
2024-04-02 03:28:13 +0530

    .

--- base/views.py
+++ base/views.py
@@ -1,16 +1,22 @@
 from django.shortcuts import render, redirect
 from .models import Room, Topic
 from .forms import RoomForm
+from django.db.models import Q
 
 def home(request):
     q = request.GET.get('q') if request.GET.get('q') != None else ''
-    rooms = Room.objects.filter(topic__name__icontains=q)
+    rooms = Room.objects.filter( 
+            Q(topic__name__icontains=q) | 
+            Q(name__icontains=q) | 
+            Q(description__icontains=q) 
+            # Q(host__icontains=q)
+    )
     
     topics = Topic.objects.all()
+    room_count=rooms.count()
     
     
-    
-    context = {'rooms': rooms, 'topics': topics}
+    context = {'rooms': rooms, 'topics': topics, "room_count": room_count}
     return render(request, 'base/home.html', context )
 
 def room(request, pk):
