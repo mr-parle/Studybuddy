019e4e169e641a5792d5201569b6532b7c762c05
mr-parle <kumarparlevinay@gmail.com>
2024-04-04 00:21:33 +0530

    .

--- base/views.py
+++ base/views.py
@@ -1,7 +1,26 @@
 from django.shortcuts import render, redirect
+from django.contrib import messages
 from .models import Room, Topic
 from .forms import RoomForm
 from django.db.models import Q
+
+
+def loginPage(request):
+    
+    if request.method == 'POST':
+        username=request.POST.get('username')
+        password=request.POST.get('password')
+        
+    try: 
+        user= User.objects.get(username = username)   
+    except:
+        messages.error(request, 'User does not exist')
+        
+        
+        
+    context={}
+    return render(request, 'base/login_register.html', context)
+
 
 def home(request):
     q = request.GET.get('q') if request.GET.get('q') != None else ''
